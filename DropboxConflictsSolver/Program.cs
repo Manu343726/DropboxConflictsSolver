e9c888029a90bcf8d8c51a911ae14fd9273f58c3
Manu343726 <Manu343726@gmail.com>
2013-10-19 11:59:39 +0200

    Authorization fixes

--- DropboxConflictsSolver/Program.cs
+++ DropboxConflictsSolver/Program.cs
@@ -13,6 +13,8 @@ namespace DropboxConflictsSolver
     {
         private static string APPNAME = "DrobpxConflictsSolver";
 
+        private static List<string> matches = new List<string>();
+
         private enum Action
         {
             DELETE,
@@ -28,8 +30,16 @@ namespace DropboxConflictsSolver
             UNKNOWN
         };
 
-        private const Action DEFAULT_ACTION = Action.SHOW;
-        private const Option DEFAULT_OPTION = Option.RECURSIVELY;
+        private enum DebugSettings
+        {
+            ALLLOG_ENABLED,
+            ALLLOG_DISABLED,
+            UNKNOWN
+        };
+
+        private const Action        DEFAULT_ACTION         = Action.SHOW;
+        private const Option        DEFAULT_OPTION         = Option.RECURSIVELY;
+        private const DebugSettings DEFAULT_DEBUG_SETTINGS = DebugSettings.ALLLOG_DISABLED;
 
         private static Action parseAction(String arg)
         {
@@ -52,6 +62,15 @@ namespace DropboxConflictsSolver
             }
         }
 
+        private static DebugSettings parseDebugSettings(String arg)
+        {
+            switch (arg)
+            {
+                case "-dl": return DebugSettings.ALLLOG_ENABLED;
+                default: return DebugSettings.UNKNOWN;
+            }
+        }
+
         private static void showHelp()
         {
             Console.WriteLine("usage: " + APPNAME + " [rootDirectory] [regex] [Action] [Option]");
@@ -65,16 +84,32 @@ namespace DropboxConflictsSolver
             Console.WriteLine("Option: Performed action options.");
             Console.WriteLine("        -r Loops directories recoursively starting at rootDirectory.");
             Console.WriteLine("        -n Search at rootDirectory only (Default).");
+            Console.WriteLine("Debug settings: Shows extra information during the search");
+            Console.WriteLine("        -dl Logs every file transversed, not only matches.");
             Console.WriteLine();
         }
 
-        private static void executeCommand(DirectoryInfo dir, Regex regex, Action action, Option option)
+        private static void executeCommand(DirectoryInfo dir, Regex regex, Action action, Option option , DebugSettings debug_settings)
         {
-            if(action == Action.HELP)
+            FileInfo[] files = null;
+            bool access_ok = false;
+
+            if (action == Action.HELP)
                 showHelp();
             else
             {
-                var files = dir.GetFiles();
+                try
+                {
+                    files = dir.GetFiles();
+                    access_ok = true;
+                }
+                catch (UnauthorizedAccessException)
+                { Console.WriteLine("ERROR: Unauthorized access to path '" + dir.FullName + "'"); }
+                catch (Exception)
+                { Console.WriteLine("ERROR: UNKNOWN"); }
+
+
+                if (!access_ok) return;
 
                 foreach (var file in files)
                 {
@@ -82,25 +117,30 @@ namespace DropboxConflictsSolver
                     {
                         Console.Write("Match: " + file.FullName + (action == Action.SHOW ? "" : " ... "));
 
+                        matches.Add(file.FullName);
+
                         if (action == Action.DELETE)
                         {
                             try { File.Delete(file.FullName); Console.WriteLine("DELETED"); }
                             catch (IOException)
                             { Console.WriteLine("ERROR: FILE IN USE"); }
                             catch (UnauthorizedAccessException)
-                            { Console.WriteLine("ERROR: UNAUTHORIZED"); }
+                            { Console.WriteLine("ERROR: Unauthorized access to file '" + file.Name + "'"); }
                             catch (Exception)
                             { Console.WriteLine("ERROR: UNKNOWN"); }
                         }
                         else
                             Console.WriteLine();
                     }
+                    else
+                        if( debug_settings == DebugSettings.ALLLOG_ENABLED )
+                            Console.WriteLine("Scanning " + file.FullName + " ... ");
                 }
 
                 if (option == Option.RECURSIVELY)
                 {
                     foreach (var subDir in dir.GetDirectories())
-                        executeCommand(subDir, regex, action, option);
+                        executeCommand(subDir, regex, action, option , debug_settings);
                 }
             }
         }
@@ -110,9 +150,10 @@ namespace DropboxConflictsSolver
             Regex regex = null;
             Action action;
             Option option;
+            DebugSettings debug_settings = 0;
             bool error = false;
 
-            if (args.Length > 1 && args.Length <= 4)
+            if (args.Length > 1 && args.Length <= 5)
             {
                 if (Directory.Exists(args[0]))
                 {
@@ -124,15 +165,21 @@ namespace DropboxConflictsSolver
 
                     if (regex != null)
                         if (args.Length == 2)
-                            executeCommand(new DirectoryInfo(args[0]), regex, DEFAULT_ACTION, DEFAULT_OPTION);
+                            executeCommand(new DirectoryInfo(args[0]), regex, DEFAULT_ACTION, DEFAULT_OPTION , DEFAULT_DEBUG_SETTINGS);
                         else
                         {
                             if ((action = parseAction(args[2])) != Action.UNKNOWN)
                                 if (args.Length == 3)
-                                    executeCommand(new DirectoryInfo(args[0]), regex, action, DEFAULT_OPTION);
+                                    executeCommand(new DirectoryInfo(args[0]), regex, action, DEFAULT_OPTION, DEFAULT_DEBUG_SETTINGS);
                                 else
                                     if ((option = parseOption(args[3])) != Option.UNKNOWN)
-                                        executeCommand(new DirectoryInfo(args[0]), regex, action, option);
+                                        if (args.Length == 4)
+                                            executeCommand(new DirectoryInfo(args[0]), regex, action, option, DEFAULT_DEBUG_SETTINGS);
+                                        else
+                                            if ((debug_settings = parseDebugSettings(args[4])) != DebugSettings.UNKNOWN)
+                                                executeCommand(new DirectoryInfo(args[0]), regex, action, option, debug_settings);
+                                            else
+                                                error = true;
                                     else
                                         error = true;
                             else
@@ -151,7 +198,14 @@ namespace DropboxConflictsSolver
                 showHelp();
             }
             else
+            {
                 Console.WriteLine("Finished.");
+
+                if (debug_settings == DebugSettings.ALLLOG_ENABLED)
+                    foreach (var match in matches)
+                        Console.WriteLine("Match: " + match);
+            }
+                
         }
     }
 }
